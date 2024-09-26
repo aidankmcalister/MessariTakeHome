54924f714a26ff275a9821fe4a77e6db87fee8b0
Aidan <aidankmcalister@gmail.com>
2024-09-26 17:58:43 -0500

    data fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import { useState, useEffect } from "react";
 import "./App.css";
+import * as d3 from "d3";
+import Plot from "./components/Plot";
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    d3.csv("./assets/messariData.csv").then(setData);
+  }, []);
   return (
-    <div>
+    <section>
       <h1 className="text-3xl font-bold underline">Messari Take Home Test</h1>
-    </div>
+      <Plot data={data} />{" "}
+    </section>
   );
 }
-
 export default App;
